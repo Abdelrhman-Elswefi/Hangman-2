96f0166d0695a0b4d8e796e48aa2d98263f7b849
Abdelrhman Elswefi <elswefy.ae@gmail.com>
2018-02-17 15:24:26 +0200

    adding a method to check if the guess is right and store the input .

--- src/com/elswefi/GameLogic.java
+++ src/com/elswefi/GameLogic.java
@@ -1,4 +1,17 @@
 package com.elswefi;
 
 public class GameLogic {
+    private String answer;
+    private String hits = "";
+    private String misses = "";
+
+    public boolean applyGuess(char letter) {
+        boolean isHit = answer.indexOf(letter) != -1;
+        if (isHit) {
+            hits += letter;
+        }else {
+            misses+=letter;
+        }
+        return isHit;
+    }
 }
